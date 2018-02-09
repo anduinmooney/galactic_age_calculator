cefe994fb2e0b9a98e60de88d7df5ee102737fdc
Anduin Mooney <anduinmooney@gmail.com>
2018-02-09 14:24:26 -0800

    Add passing mercury test.

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -14,6 +14,6 @@ describe('mercuryAge', function() {
   it('should correctly convert years into seconds and multiply seconds to mercurys solar year', function() {
     let earthAge = 2;
     const yearToSeconds = 3153600;
-    expect(earthAge * yearToSeconds * .24).toEqual(1513727);
+    expect(earthAge * yearToSeconds * .24).toEqual(1513728);
   });
 });
