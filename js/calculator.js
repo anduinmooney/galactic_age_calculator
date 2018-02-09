507b6d95eb597a35d3aae9b67a8fdc27e788f339
Anduin Mooney <anduinmooney@gmail.com>
2018-02-09 14:24:09 -0800

    Add failing mercury test.

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,14 @@
-export function secondsInYear(earthAge) {
-	const yearToSeconds = 31536000;
-	let ageConvertedIntoSeconds = earthAge * yearToSeconds;
-	return ageConvertedIntoSeconds;
+export class Age {
+
+  secondsInYear(earthAge) {
+	   const yearToSeconds = 31536000;
+	    let ageConvertedIntoSeconds = earthAge * yearToSeconds;
+	     return ageConvertedIntoSeconds;
+     }
+
+  mercuryAge(earthAge){
+      const yearToSeconds = 31536000;
+      let ageConvertedIntoMercuryAge = earthAge * yearToSeconds * .24;
+       return ageConvertedIntoMercuryAge;
+     }
 }
--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,4 +1,4 @@
-import { secondsInYear } from './../js/calculator.js';
+import { Age } from './../js/calculator.js';
 
 describe('secondsInYear', function() {
 
@@ -6,5 +6,14 @@ describe('secondsInYear', function() {
     let earthAge = 2;
     const yearToSeconds = 3153600;
     expect(earthAge * yearToSeconds).toEqual(6307200);
+  });
+});
+
+describe('mercuryAge', function() {
+
+  it('should correctly convert years into seconds and multiply seconds to mercurys solar year', function() {
+    let earthAge = 2;
+    const yearToSeconds = 3153600;
+    expect(earthAge * yearToSeconds * .24).toEqual(1513727);
   });
 });
